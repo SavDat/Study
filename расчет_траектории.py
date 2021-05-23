d72703d771fd30218cd197da83e042a5d58cce20
SavDat <84165588+SavDat@users.noreply.github.com>
2021-05-23 16:08:52 +0500

    Расчет траектории 02

--- расчет_траектории.py
+++ расчет_траектории.py
@@ -4,17 +4,17 @@ x_array = np.array(input().split(), dtype=float)
 h_array = np.array(input().split(), dtype=float)
 x_target = float(input())
 h_target = float(input())
-a = np.polyfit(x_array, h_array, 2)
+
+a = np.polyfit(x_array, h_array, 2)  # нахождение коэфициентов для функции
 
 
 def get_trend(x, a):
-    y = a[0] * x**2 + a[1] * x + a[2]
+    y = a[0] * x**2 + a[1] * x + a[2]  # расчет высот траектории по функции параболы
     return y
 
 
 h_zero = get_trend(0, a)  # высота расположения пушки
-h_target_get = get_trend(x_target, a)
+h_target_get = get_trend(x_target, a)  # расчетная высота расположения цели
+delta_h = abs(h_target - h_target_get)  # расхождение высот расположения цели
 
-
-delta_h = abs(h_target - h_target_get)
 print("h0 = %6.2f, %2s" % (h_zero, 'no' if delta_h > 0.5 else 'yes'))
